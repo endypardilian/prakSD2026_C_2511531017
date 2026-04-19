d05036bfc948f8239d913cb3e595df66ffdb28b2
endypardilian <endy.npd@gmail.com>
2026-04-19 14:43:43 +0700

    Praktikum_Pekan3_2511531017

---
+++ PrakSD2026_C_2511531017/src/pekan3_2511531017/Website_2511531017.java
@@ -0,0 +1,23 @@
+package pekan3_2511531017;
+
+public class Website_2511531017 {
+    private String judul_1017;
+    private String url_1017;
+
+    public Website_2511531017(String judul, String url) {
+        this.judul_1017 = judul;
+        this.url_1017 = url;
+    }
+    public String getJudul_1017() {
+        return judul_1017;
+    }
+    public String getUrl_1017() {
+        return url_1017;
+    }
+    public void setJudul_1017(String judul) {
+        this.judul_1017 = judul;
+    }
+    public void setUrl_1017(String url) {
+        this.url_1017 = url;
+    }
+}
