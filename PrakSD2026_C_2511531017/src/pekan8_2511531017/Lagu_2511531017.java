777718488b68d13e9bddd52b31fffe9455633921
endypardilian <endy.npd@gmail.com>
2026-06-01 22:47:33 +0700

    Tugas_Praktikum_Pekan8_2511531017

---
+++ PrakSD2026_C_2511531017/src/pekan8_2511531017/Lagu_2511531017.java
@@ -0,0 +1,12 @@
+package pekan8_2511531017;
+
+public class Lagu_2511531017 {
+    String judul_1017;
+    String penyanyi_1017;
+    int durasi_1017;
+    public Lagu_2511531017(String judul_1017, String penyanyi_1017, int durasi_1017) {
+        this.judul_1017 = judul_1017;
+        this.penyanyi_1017 = penyanyi_1017;
+        this.durasi_1017 = durasi_1017;
+    }
+}
